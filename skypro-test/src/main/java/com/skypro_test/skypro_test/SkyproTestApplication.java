5c9a6ad7a40fb02b1c91d8410ce40789483d0659
Moisuk-Dmitri <moisuk.d@gmail.com>
2024-10-01 16:55:06 +0300

    Initial commit

---
+++ skypro-test/src/main/java/com/skypro_test/skypro_test/SkyproTestApplication.java
@@ -0,0 +1,13 @@
+package com.skypro_test.skypro_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproTestApplication.class, args);
+	}
+
+}
